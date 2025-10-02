0d5b825e2336b020d8749db245f5f347f17693b2
ale.mosca <ale.mosca@nexidigital.eu>
2025-10-02 20:18:17 +0200

    test eternita

--- src/lib/auth-utils.ts
+++ src/lib/auth-utils.ts
@@ -17,7 +17,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 
-// Prendi le variabili lato server, fallback a quelle PUBLIC se necessario
+
 const supabaseUrl = process.env.SUPABASE_URL! 
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY! 
 
