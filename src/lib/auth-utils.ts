40a6cf1f2f46b98e68b01d481925649eb370b8a5
ale.mosca <ale.mosca@nexidigital.eu>
2025-10-02 19:42:00 +0200

    last commit of the day

--- src/lib/auth-utils.ts
+++ src/lib/auth-utils.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseAdmin = createClient(
-  process.env.PUBLIC_SUPABASE_URL!,
+  // process.env.PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY! 
 );
 
