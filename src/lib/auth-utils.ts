87a1273be7e6a20556ff1ca7208e367e1ee63fee
ale.mosca <ale.mosca@nexidigital.eu>
2025-10-05 12:27:28 +0200

    deploy retest

--- src/lib/auth-utils.ts
+++ src/lib/auth-utils.ts
@@ -1,11 +1,28 @@
+// import { createClient } from '@supabase/supabase-js';
+
+// const supabaseAdmin = createClient(
+//     process.env.PUBLIC_SUPABASE_URL!,
+//     process.env.SUPABASE_SERVICE_ROLE_KEY! 
+// );
+
+// export async function verifyTokenWithSupabase(accessToken: string) {
+//   const { data, error } = await supabaseAdmin.auth.getUser(accessToken);
+//   if (error || !data.user) throw new Error('Token non valido');
+//   console.log('User:', data.user);
+//   return data.user;
+// }
+
+
+
 import { createClient } from '@supabase/supabase-js';
 
+if (!process.env.PUBLIC_SUPABASE_URL || !process.env.SUPABASE_SERVICE_ROLE_KEY) {
+  throw new Error('⚠️ Missing Supabase environment variables for server-side usage.');
+}
+
 const supabaseAdmin = createClient(
-  //  process.env.PUBLIC_SUPABASE_URL!,
-  //  process.env.SUPABASE_SERVICE_ROLE_KEY! 
-  import.meta.env.PUBLIC_SUPABASE_URL,
-  import.meta.env.PUBLIC_SUPABASE_ANON_KEY,
-   
+  process.env.PUBLIC_SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 export async function verifyTokenWithSupabase(accessToken: string) {
@@ -13,6 +30,3 @@ export async function verifyTokenWithSupabase(accessToken: string) {
   if (error || !data.user) throw new Error('Token non valido');
   return data.user;
 }
-
-
-
