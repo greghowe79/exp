26e3b2b5e356258224f5c1ec778196733739a6be
ale.mosca <ale.mosca@nexidigital.eu>
2025-10-05 12:06:59 +0200

    cambiati import per variabile di ambiente supabase

--- src/lib/auth-utils.ts
+++ src/lib/auth-utils.ts
@@ -1,8 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseAdmin = createClient(
-  process.env.PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY! 
+  //  process.env.PUBLIC_SUPABASE_URL!,
+  //  process.env.SUPABASE_SERVICE_ROLE_KEY! 
+  import.meta.env.PUBLIC_SUPABASE_URL,
+  import.meta.env.PUBLIC_SUPABASE_ANON_KEY,
+   
 );
 
 export async function verifyTokenWithSupabase(accessToken: string) {
